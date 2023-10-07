3c06f5c948a4b38eee55f6ae828b8d00338c8a8b
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2023-10-07 10:01:30 +0700

    Update page home

--- SocialNetworkFE/src/pages/Home/index.jsx
+++ SocialNetworkFE/src/pages/Home/index.jsx
@@ -1,9 +1,15 @@
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
+import CreatePost from "~/components/form/CreatePost/CreatePost";
+import Post from "~/components/Post/Post";
 function Home() {
     const cx = classNames.bind(styles);
     return ( 
         <div className={cx('wrapper')}>
+            <div className={cx('bg-sidebar','wrapper__createPost')}><CreatePost/></div>
+            <div className={cx('wrapper_listPost')}>
+                <Post/>
+            </div>
         </div>
     );
 }
