29a91c06bb66a29bad49355146e49d97e66d7427
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2023-10-13 22:02:41 +0700

    update Profile Page

--- SocialNetworkFE/src/components/layouts/ProfileLayout/ProfileLayout.jsx
+++ SocialNetworkFE/src/components/layouts/ProfileLayout/ProfileLayout.jsx
@@ -1,11 +1,38 @@
 import classNames from "classnames/bind";
+import { useEffect, useState } from "react";
 import styles from './ProfileLayout.module.scss';
 import Header from "../components/Header";
+import images from "~/assets/images";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 function ProfileLayout({children}) {
     const cx = classNames.bind(styles);
+    const [valueOnScrollTopWindow,setOnScrollTopWindow] = useState(0);
+    // SET SCROLL TOP FOR VALUE ON SCROLL THEN SEND THE DATA TO COMPONENTS HEADER
+    const handleOnScrollWindow = (e) => {
+        setOnScrollTopWindow(e.target.scrollTop);
+    };
+    const handleClickBackToTop = ()=> {
+        window.scrollTo(0, 0);
+    };
+    useEffect(()=>{
+        // 👇️ scroll to top on page load
+        window.scrollTo(0, 0);
+    },[])
     return (  
-        <div className={cx('wrapper','w-full h-full')}>
+        <div className={cx('wrapper','w-full h-full')} onScroll={(e) => handleOnScrollWindow(e)}>
             <Header/>
+            {
+                valueOnScrollTopWindow > 470 ? (
+                    <div className={cx('wrapper__headerUser','flex items-center justify-between bg-black text-white')}>
+                        <div className={cx('flex items-center','wrapper__headerUser-box')} onClick={handleClickBackToTop}>
+                            <img className={cx('w-16 h-16 rounded-full')} src={images.user} alt="user"/>
+                            <span>Nguyen Thanh Quynh Linh</span>
+                        </div>
+                        <FontAwesomeIcon className={cx('wrapper__headerUser-icon')} icon={faEllipsisVertical}/>
+                    </div>
+                ) : ''
+            }
             <div className={cx('wrapper__container','w-full h-full')}>
                 {children}
             </div>
--- SocialNetworkFE/src/pages/Profile/components/HomeProfile/HomeProfile.jsx
+++ SocialNetworkFE/src/pages/Profile/components/HomeProfile/HomeProfile.jsx
@@ -1,20 +1,134 @@
 import classNames from "classnames/bind";
 import styles from "./HomeProfile.module.scss";
 import Post from "~/components/Post/Post";
+import {ALL_FRIENDS_USER, ALL_IMAGES_USER, DATA__PERSONAL__INFORMATION as personalInfo } from "~/const/data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {Button as ButtonEdit} from "~/components/button/button";
+import { Link } from "react-router-dom";
+import CreatePost from "~/components/form/CreatePost/CreatePost";
+
 function HomeProfile() {
     const cx = classNames.bind(styles);
+
     return (  
-        <div className={cx('wrapper','flex')}>
+        <div className={cx('wrapper','flex')} >
             <div className={cx('wrapper__left','w-2/6')}>
-                <div className={cx('wrapper__left-info','bg-sidebar mt-15 text-white')}>
+                <div className={cx('wrapper__left-info','bg-sidebar mt-5 text-white')}>
                     <span className={cx('wrapper__left-info-title')}>Giới thiệu</span>
                     <div className={cx('wrapper__left-info-introduce','text-center')}>
                         <div className={cx('wrapper__left-info-introduce-des')}>yen</div>
-                        <button type="button">Chỉnh sữa tiểu sử</button>
+                        <ButtonEdit type="button" content="Chỉnh sữa tiểu sử"/>
+                    </div>
+                    <ul className={cx('wrapper__left-info-list')}>    
+                        <li>
+                            {
+                                personalInfo.university && personalInfo.university.status === 'Đang học' ? 
+                                (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')}  icon={personalInfo.university.icon}/>
+                                        <span className="pl-5">Học tại {personalInfo.university.name}</span>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')} icon={personalInfo.university.icon}/>
+                                        <span className="pl-5">Đã học tại {personalInfo.university.name}</span>
+                                    </div>
+                                )
+                            }
+                        </li>
+                        <li>
+                            {
+                                 personalInfo.hightSchool && personalInfo.hightSchool.status === 'Đã từng' ? 
+                                (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')} icon={personalInfo.hightSchool.icon}/>
+                                        <span className="pl-5">Đã học tại {personalInfo.hightSchool.name}</span>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')} icon={personalInfo.hightSchool.icon}/>
+                                        <span className="pl-5">Học tại {personalInfo.hightSchool.name}</span>
+                                    </div>
+                                )
+                            }
+                        </li>
+                        <li>
+                            {
+                                personalInfo.from && personalInfo.from.name !== '' ? 
+                                (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')} icon={personalInfo.from.icon}/>
+                                        <span className="pl-5">Sống tại {personalInfo.from.name}</span>
+                                    </div>
+                                ) : ''
+                            }
+                        </li>
+                        <li>
+                            {
+                                personalInfo.liveAt && personalInfo.liveAt.name !== '' ? 
+                                (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')} icon={personalInfo.liveAt.icon}/>
+                                        <span className="pl-5">Đến từ {personalInfo.liveAt.name}</span>
+                                    </div>
+                                ) : ''
+                            }
+                        </li>
+                        <li>
+                            {
+                                personalInfo.matrimony && personalInfo.matrimony.name === 'Hẹn hò' ? 
+                                (
+                                    <div>
+                                        <FontAwesomeIcon className={cx('wrapper__left-info-list-itemIcon')} icon={faHeart}/>
+                                        <span className="pl-5">{personalInfo.matrimony.name}</span>
+                                    </div>
+                                ) : ''
+                            }
+                        </li>
+                    </ul>
+                    <ButtonEdit type="button" content="Chỉnh sữa tiểu sử"/>
+                    <ButtonEdit type="button" className="mt-5" content="Thêm sở thích"/>
+                </div>
+                <div className={cx('wrapper__left-imagesUser','bg-sidebar mt-5 text-white')}>
+                    <div className={cx('flex items-center justify-between')}>
+                        <span className={cx('wrapper__left-imagesUser-title')}>Ảnh</span>
+                        <Link to='/profile/photos'>Xem tất cả ảnh</Link>
+                    </div>
+                    <div className={cx('wrapper__left-imagesUser-list','mt-5')}>
+                        <div className="grid grid-rows-3 grid-flow-col gap-3">
+                            {
+                                ALL_IMAGES_USER.images.map((item,index) =>{
+                                    return <img key={index} className={cx('w-40 h-40 object-cover')} src={item} alt="all image"/>
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('wrapper__left-friendsUser','bg-sidebar mt-5 text-white')}>
+                    <div className={cx('flex items-center justify-between')}>
+                        <span className={cx('wrapper__left-friendsUser-title')}>Bạn bè</span>
+                        <Link to='/profile/photos'>Xem tất cả bạn bè</Link>
+                    </div>
+                    <div className={cx('wrapper__left-friendsUser-quantityFriends','text-color-text')}>269 bạn bè</div>
+                    <div className={cx('wrapper__left-friendsUser-list','mt-5')}>
+                        <div className="grid grid-rows-3 grid-flow-col gap-3">
+                            {
+                                ALL_FRIENDS_USER.map((item,index) =>{
+                                    return (
+                                        <div key={index}>
+                                            <img key={index} className={cx('w-40 h-40 object-cover')} src={item.image} alt="all image"/>
+                                            <div className={cx('w-full','wrapper__left-friendsUser-list-itemName')}>{item.name}</div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
             <div className={cx('wrapper__right','w-4/6')}>
+                <div className={cx('wrapper__right-createPost','bg-sidebar')}><CreatePost/></div>
                 <Post/>
             </div>
         </div>
