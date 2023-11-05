0d7f998af0b78a41e75075b55272c1e46799bac2
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2023-11-05 18:20:53 +0700

    Update code (create db post in fe and be)

---
+++ SocialNetWorkBE/src/main/java/com/socialnetwork/SocialNetWork/repository/PostRepository.java
@@ -0,0 +1,2 @@
+package com.socialnetwork.SocialNetWork.repository;public interface PostRepository {
+}
---
+++ SocialNetWorkBE/src/main/java/com/socialnetwork/SocialNetWork/service/PostService.java
@@ -0,0 +1,2 @@
+package com.socialnetwork.SocialNetWork.service;public interface PostService {
+}
