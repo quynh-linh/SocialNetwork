f6ffbc94d1310930596e3e51d554a492d0ba7ea8
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2023-11-24 01:07:26 +0700

    Update get list post fe

---
+++ SocialNetWorkBE/src/main/java/com/socialnetwork/SocialNetWork/model/IMPL/PostById.java
@@ -0,0 +1,9 @@
+package com.socialnetwork.SocialNetWork.model.IMPL;
+
+public interface MediaByPost {
+    String getId();
+    String getPostId();
+    String getMediaId();
+    String getMediaUrl();
+    String getMedia_type();
+}
